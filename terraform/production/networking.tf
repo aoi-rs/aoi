1f0b0a2522cd3cca53299454d8b0bef06f0775f3
Davi Costa de Melo <davicosta01082008@gmail.com>
2026-05-25 15:12:52 -0300

    chore(terraform): add missing AWS route table associations

--- terraform/production/networking.tf
+++ terraform/production/networking.tf
@@ -43,7 +43,15 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
 
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
 
 resource "aws_security_group" "alb" {
   vpc_id = aws_vpc.main.id
