f61fbdfc960ff2f12a2cfccf42004eeffd242356
Davi Costa de Melo <davicosta01082008@gmail.com>
2026-05-24 23:02:14 -0300

    chore(terraform): format `global/main.tf`

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -8,17 +8,17 @@ resource "tfe_variable_set" "global" {
 }
 
 resource "tfe_variable" "tfc_aws_provider_auth" {
-  key      = "TFC_AWS_PROVIDER_AUTH"
-  value    = "true"
-  category = "env"
+  key             = "TFC_AWS_PROVIDER_AUTH"
+  value           = "true"
+  category        = "env"
   variable_set_id = tfe_variable_set.global.id
-  description = "Enable the Workload Identity integration for AWS"
+  description     = "Enable the Workload Identity integration for AWS"
 }
 
 resource "tfe_variable" "tfc_aws_run_role_arn" {
-  key = "TFC_AWS_RUN_ROLE_ARN"
-  value = "arn:aws:iam::891924440815:role/terraform-cloud"
-  category = "env"
+  key             = "TFC_AWS_RUN_ROLE_ARN"
+  value           = "arn:aws:iam::891924440815:role/terraform-cloud"
+  category        = "env"
   variable_set_id = tfe_variable_set.global.id
-  description = "The AWS role arn runs will use to authenticate"
+  description     = "The AWS role arn runs will use to authenticate"
 }
