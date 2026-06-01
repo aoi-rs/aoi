ac65534786cb5a49326c375c3d003cadb1c17071
Davi Costa de Melo <davicosta01082008@gmail.com>
2026-05-31 22:15:54 -0300

    refactor(terraform): remove unused aws variables

--- terraform/production/variables.tf
+++ terraform/production/variables.tf
@@ -3,15 +3,3 @@ variable "service_secret_production" {
   sensitive   = true
   type        = string
 }
-
-variable "aws_access_key_id_production" {
-  description = "AWS Access Key ID for production"
-  sensitive   = true
-  type        = string
-}
-
-variable "aws_secret_access_key_production" {
-  description = "AWS Secret Access Key for production"
-  sensitive   = true
-  type        = string
-}
