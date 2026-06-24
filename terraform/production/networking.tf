ab11ad3303727a19cc95d007630be5a0280cbbcd
Davi Costa de Melo <davicosta01082008@gmail.com>
2026-06-24 01:44:52 -0300

    chore(terraform): add HTTP ingress to internal ALB

--- terraform/production/networking.tf
+++ terraform/production/networking.tf
@@ -88,6 +88,13 @@ resource "aws_security_group" "internal_alb" {
   name   = "aoi-internal-alb"
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
