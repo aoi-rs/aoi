9adfeaa8fe9a4aad9235ef9f0fe086badeaed285
Davi Melo <davimelo01082008@gmail.com>
2026-08-20 17:47:37 -0300

    chore(terraform): add internet-facing ALB for redirector

--- terraform/production/alb.tf
+++ terraform/production/alb.tf
@@ -16,15 +16,30 @@ resource "aws_alb" "service" {
 resource "aws_alb" "redirector" {
   name               = "redirector"
   load_balancer_type = "application"
-  internal           = false
+  internal           = true
 
   security_groups = [
     aws_security_group.redirector_alb.id
   ]
 
   subnets = [
-    aws_subnet.public_a.id,
-    aws_subnet.public_b.id,
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id,
+  ]
+}
+
+resource "aws_alb" "redirector_test" {
+  name = "redirector-test"
+  load_balancer_type = "application"
+  internal = false
+
+  security_groups = [
+    aws_security_group.service_alb.id
+  ]
+
+  subnets = [
+    aws_subnet.public_a,
+    aws_subnet.public_b,
   ]
 }
 
@@ -52,6 +67,18 @@ resource "aws_alb_target_group" "redirector" {
   }
 }
 
+resource "aws_alb_target_group" "redirector_test" {
+  vpc_id = aws_vpc.main.id
+  name = "redirector-test"
+  port = 12000
+  protocol = "HTTP"
+  target_type = "ip"
+
+  health_check {
+    path = "/"
+  }
+}
+
 resource "aws_alb_listener" "http" {
   load_balancer_arn = aws_alb.service.arn
   port              = 80
@@ -68,6 +95,8 @@ resource "aws_alb_listener" "http" {
   }
 }
 
+
+
 locals {
   load_balancers = {
     main = {
@@ -78,6 +107,11 @@ locals {
     internal = {
       arn              = aws_alb.redirector.arn
       target_group_arn = aws_alb_target_group.redirector.arn
+    }
+
+    test = {
+      arn = aws_alb.redirector_test.arn
+      target_group_arn = aws_alb_target_group.redirector_test.arn
     }
   }
 }
--- terraform/production/networking.tf
+++ terraform/production/networking.tf
@@ -88,25 +88,11 @@ resource "aws_security_group" "redirector_alb" {
   name   = "redirector-alb"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront.id]
   }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # ingress {
-  #   from_port       = 443
-  #   to_port         = 443
-  #   protocol        = "tcp"
-  #   prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront.id]
-  # }
 
   egress {
     from_port   = 0
@@ -143,7 +129,8 @@ resource "aws_security_group" "redirector" {
     from_port       = 12000
     to_port         = 12000
     protocol        = "tcp"
-    security_groups = [aws_security_group.redirector_alb.id]
+    # TODO: remove service ALB after the load test
+    security_groups = [aws_security_group.redirector_alb.id, aws_security_group.service_alb.id]
   }
 
   egress {
